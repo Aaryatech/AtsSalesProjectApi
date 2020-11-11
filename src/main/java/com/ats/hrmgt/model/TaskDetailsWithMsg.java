109b6edc2ee7360494edaff9649c1f16108ffe22
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-11-11 10:27:49 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/model/TaskDetailsWithMsg.java
+++ src/main/java/com/ats/hrmgt/model/TaskDetailsWithMsg.java
@@ -1,81 +1,64 @@
 package com.ats.hrmgt.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class TaskDetailsWithMsg {
 
-	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int taskId;
-	
-	
-	private String taskDoneDate;
-	
-	
-	
+
+	private Date taskDoneDate;
+
 	private String message;
 
-
 	private String empName;
-
 
 	public int getTaskId() {
 		return taskId;
 	}
 
-
 	public void setTaskId(int taskId) {
 		this.taskId = taskId;
 	}
 
-
-	public String getTaskDoneDate() {
+	@JsonFormat(locale = "Locale.ENGLISH", timezone = "Asia/Kolkata", pattern = "dd-MM-yyyy hh:mm:ss a")
+	public Date getTaskDoneDate() {
 		return taskDoneDate;
 	}
 
-
-	public void setTaskDoneDate(String taskDoneDate) {
+	public void setTaskDoneDate(Date taskDoneDate) {
 		this.taskDoneDate = taskDoneDate;
 	}
- 
+
 	public String getMessage() {
 		return message;
 	}
-
 
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-
 	public String getEmpName() {
 		return empName;
 	}
 
-
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
-
 
 	@Override
 	public String toString() {
 		return "TaskDetailsWithMsg [taskId=" + taskId + ", taskDoneDate=" + taskDoneDate + ", message=" + message
 				+ ", empName=" + empName + "]";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
